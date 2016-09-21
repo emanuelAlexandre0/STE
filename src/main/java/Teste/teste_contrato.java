a612f3c64e138e0cf9e7862a25303edc6258e8f3
franciele <franciferreiraap@gmail.com>
2016-09-21 18:16:56 -0300

    Persitence

--- src/main/java/Teste/teste_contrato.java
+++ src/main/java/Teste/teste_contrato.java
@@ -1,6 +1,7 @@
-package teste;
+package Teste;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,14 +14,15 @@ import br.com.avansus.model.Endereco;
 import br.com.avansus.model.Telefone;
 import br.com.avansus.model.Viagem;
 
-public class teste_cliente {
+public class teste_contrato {
 	public static void main(String[] args){
+		
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("STEPU");
 		EntityManager manager = factory.createEntityManager();
 		
 		EntityTransaction trx = manager.getTransaction();
 		trx.begin();
-		
+	
 		ArrayList<Endereco> enderecos = new ArrayList<>();
 		ArrayList<Telefone> telefones = new ArrayList<>();
 		ArrayList<Viagem> viagens = new ArrayList<>();
@@ -28,36 +30,49 @@ public class teste_cliente {
 		
 		Endereco endereco = new Endereco();
 		endereco.setTipo_envolvido("C");
-		endereco.setEndereco("Rua dos Ypês, Eldorado, Lavras MG");
-		endereco.setComplemento("Casa");
+		endereco.setEndereco("Rua dos Guararipes, NS Fatima, Lavras MG");
+		endereco.setComplemento("Comercial");
 		endereco.setCep("37200-000");
 		enderecos.add(endereco);
 		
 		Telefone telefone = new Telefone();
 		telefone.setCod_pais(55);
 		telefone.setCod_area(35);
-		telefone.setNumero("998390936");
-		telefone.setNome_contado("Emanuel");
+		telefone.setNumero("991231234");
+		telefone.setNome_contado("Juliano");
 		telefones.add(telefone);
 		
 		Cliente cliente = new Cliente();
-		cliente.setNome("Emanuel Alexandre");
+		cliente.setNome("Juliano Alvarez");
 		cliente.setTipo("F");
-		cliente.setCpfcnpj("213.221.167-17");
-		cliente.setRazao("Emanuel Alexandre");
-		cliente.setEmail_principal("emanuel@gmail.com");
-		cliente.setEmail_secundario("emanuel.alexandre@gmail.com");
+		cliente.setCpfcnpj("938.129.123-03");
+		cliente.setRazao("Comercial Juliano");
+		cliente.setEmail_principal("juliano@gmail.com");
+		cliente.setEmail_secundario("comercial_juliano@gmail.com");
 		cliente.setStatus("Ativo");
 		cliente.setEnderecos(enderecos);
+				
+		Contrato contrato = new Contrato();
+		contrato.setCliente(cliente);
+		contrato.setCodigo("00001");
+		contrato.setDados_adicionais("");
+		//contrato.setData_assinatura();
+		//contrato.setData_validade();
+		contrato.setDados_adicionais("Contrato de teste");
+		contrato.setStatus("A");
+		contrato.setTempo_min_solicitacao(45);
+		
 		
 		manager.persist(enderecos);
 		manager.persist(viagens);
 		manager.persist(telefones);
 		manager.persist(contratos);
 		manager.persist(cliente);
+		manager.persist(contrato);
+				
 		
 		trx.commit();
 		manager.close();
 	}
-	
+
 }
--- src/main/java/Teste/teste_servico.java
+++ src/main/java/Teste/teste_servico.java
@@ -1,4 +1,4 @@
-package teste;
+package Teste;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
